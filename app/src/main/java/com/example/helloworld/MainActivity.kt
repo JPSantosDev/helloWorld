f015083cbe4db12a727d98e9e85f6845e43b64c2
Carolbrasill <carolbrasil.019@gmail.com>
2026-05-25 17:09:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val senhaCorreta = 1234;
+
+        val textoOla = findViewById<TextView>(R.id.textView2)
+        val textoEdit = findViewById<TextView>(R.id.editTextNumberPassword)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            if(textoEdit.text.toString() == senhaCorreta.toString()){
+                textoOla.text = "Senha correta"
+            }
+            else{
+                textoOla.text = "Senha Incorreta"
+            }
+        }
+    }
+}
